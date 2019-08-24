e1a95c4aa3ea09059dd817a7e923b8fc344d0252
mihovu <hoangvmh@gmail.com>
2019-08-25 08:58:34 +0900

    Add splash screen

---
+++ app/src/main/java/giavu/co/jp/connpassx/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package giavu.co.jp.connpassx.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import giavu.co.jp.connpassx.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
