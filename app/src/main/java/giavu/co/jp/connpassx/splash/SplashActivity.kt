b2b07c8495e5597a3b8a3752809907f02b4be17a
mihovu <hoangvmh@gmail.com>
2019-08-25 14:15:09 +0900

    Add logo and splash screen

--- app/src/main/java/giavu/co/jp/connpassx/splash/SplashActivity.kt
+++ app/src/main/java/giavu/co/jp/connpassx/splash/SplashActivity.kt
@@ -1,13 +1,27 @@
 package giavu.co.jp.connpassx.splash
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import giavu.co.jp.connpassx.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        initializeView()
+    }
+
+    private fun initializeView() {
+        app_name.startAnimation(
+            AnimationUtils.loadAnimation(
+                this,
+                R.anim.transition_up
+            ).apply {
+                startOffset = 600L
+            }
+        )
     }
 }
