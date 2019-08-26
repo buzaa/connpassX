967d722020474a2be91c1007e6636ee9757160ac
mihovu <hoangvmh@gmail.com>
2019-08-27 08:46:29 +0900

    Remove text

--- app/src/main/java/giavu/co/jp/connpassx/splash/SplashActivity.kt
+++ app/src/main/java/giavu/co/jp/connpassx/splash/SplashActivity.kt
@@ -1,10 +1,8 @@
 package giavu.co.jp.connpassx.splash
 
 import android.os.Bundle
-import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import giavu.co.jp.connpassx.R
-import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
@@ -15,13 +13,6 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun initializeView() {
-        app_name.startAnimation(
-            AnimationUtils.loadAnimation(
-                this,
-                R.anim.transition_up
-            ).apply {
-                startOffset = 600L
-            }
-        )
+        
     }
 }
