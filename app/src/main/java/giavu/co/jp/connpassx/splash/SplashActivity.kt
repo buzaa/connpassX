b6fc60bb71744fe46de7f97c40bec7afec269b0b
mihovu <hoangvmh@gmail.com>
2019-08-29 08:15:15 +0900

    add image in screen

--- app/src/main/java/giavu/co/jp/connpassx/splash/SplashActivity.kt
+++ app/src/main/java/giavu/co/jp/connpassx/splash/SplashActivity.kt
@@ -2,17 +2,30 @@ package giavu.co.jp.connpassx.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import giavu.co.jp.connpassx.R
+import org.koin.android.ext.android.inject
 
 class SplashActivity : AppCompatActivity() {
+
+    private val viewModel: SplashViewModel by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        initializeView()
+        initialize()
+        observeViewModel()
     }
 
-    private fun initializeView() {
+    private fun initialize() {
 
+    }
+
+    private fun observeViewModel() {
+        with(viewModel) {
+            seriesEvent.observe(this@SplashActivity, Observer {
+
+            })
+        }
     }
 }
