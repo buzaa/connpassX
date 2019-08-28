a1604910389f9965324cfb3912f8937607b2e867
mihovu <hoangvmh@gmail.com>
2019-08-29 08:14:55 +0900

    Add splashviewmodel

---
+++ app/src/main/java/giavu/co/jp/connpassx/splash/SplashViewModel.kt
@@ -0,0 +1,42 @@
+package giavu.co.jp.connpassx.splash
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import giavu.co.jp.domain.usecase.FetchConnpassEventUseCase
+import giavu.co.jp.repository.model.Series
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.rx2.await
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+
+/**
+ * @Author: Hoang Vu
+ * @Date:   2019-08-28
+ */
+class SplashViewModel(
+    private val fetchConnpassEventUseCase: FetchConnpassEventUseCase
+) : ViewModel() {
+
+    private val _seriesEvent = MutableLiveData<List<Series>>()
+    val seriesEvent: LiveData<List<Series>>
+        get() = _seriesEvent
+
+    fun fetchSeries() {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                withContext(Dispatchers.IO) {
+                    fetchConnpassEventUseCase.fetchSeries().await()
+                }
+            }.onSuccess {
+                _seriesEvent.value = it
+            }.onFailure {
+                Timber.e(it)
+            }
+        }
+
+    }
+
+}
