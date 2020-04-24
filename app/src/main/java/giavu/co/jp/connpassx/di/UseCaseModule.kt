e912c8049ab20477887294096f2ee73b398d7d40
vegeta2102 <hoangvmh@gmail.com>
2020-04-24 17:27:10 +0900

    fix use case module

--- app/src/main/java/giavu/co/jp/connpassx/di/UseCaseModule.kt
+++ app/src/main/java/giavu/co/jp/connpassx/di/UseCaseModule.kt
@@ -1,14 +1,15 @@
 package giavu.co.jp.connpassx.di
 
 import giavu.co.jp.domain.usecase.FetchConnpassEventUseCase
-import org.koin.dsl.module.Module
+import org.koin.core.module.Module
+import org.koin.dsl.module
 
 /**
  * @Author: Hoang Vu
  * @Date:   2019-08-21
  */
 class UseCaseModule {
-    val module: Module = org.koin.dsl.module.module {
+    val module: Module = module {
         single { FetchConnpassEventUseCase(connpassApi = get()) }
     }
 }
