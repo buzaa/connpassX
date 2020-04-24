6ccddc1094f440a59e14810f79e81b940604d60d
vegeta2102 <hoangvmh@gmail.com>
2020-04-24 17:22:04 +0900

    update koin

--- app/src/main/java/giavu/co/jp/connpassx/application/KoinInitializer.kt
+++ app/src/main/java/giavu/co/jp/connpassx/application/KoinInitializer.kt
@@ -3,9 +3,10 @@ package giavu.co.jp.connpassx.application
 import android.app.Application
 import giavu.co.jp.connpassx.BuildConfig
 import giavu.co.jp.connpassx.di.Modules
-import org.koin.android.ext.android.startKoin
+import org.koin.android.ext.koin.androidContext
 import org.koin.android.logger.AndroidLogger
-import org.koin.log.EmptyLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.EmptyLogger
 
 /**
  * @Author: Hoang Vu
@@ -14,14 +15,15 @@ import org.koin.log.EmptyLogger
 class KoinInitializer(private val application: Application) {
 
     fun initialize() {
-        application.startKoin(
-                androidContext = application,
-                modules = Modules().get(),
-                logger = if (BuildConfig.DEBUG) {
-                    AndroidLogger()
-                } else {
-                    EmptyLogger()
-                }
-        )
+        startKoin {
+            if (BuildConfig.DEBUG) {
+                AndroidLogger()
+            } else {
+                EmptyLogger()
+            }
+            androidContext(application)
+            modules(Modules().get())
+        }
+
     }
 }
